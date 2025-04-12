2324cb619a844336cc4e8b040eb5ca5feadeb4b4
prince <princekumar7320918928@gmail.com>
2025-04-12 19:52:45 +0530

    upd

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,7 @@ const fileUpload=require("express-fileupload")
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
+  // origin:"https://code-boost-s1tf.vercel.app",  // Replace with your frontend URL
   origin:"http://localhost:5173",  // Replace with your frontend URL
   credentials: true                // Allow cookies to be sent
 }));
