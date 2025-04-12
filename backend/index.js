a5d21845beec5e96d6ac9d448b5790297b56c572
prince <princekumar7320918928@gmail.com>
2025-04-12 19:39:11 +0530

    upd

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,8 @@ const fileUpload=require("express-fileupload")
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-  origin:"http://localhost:5173",  // Replace with your frontend URL
+  origin:"https://code-boost-s1tf.vercel.app",  // Replace with your frontend URL
+  // origin:"http://localhost:5173",  // Replace with your frontend URL
   credentials: true                // Allow cookies to be sent
 }));
 app.use(cookieParser()); // We use cookieParser to parse cookies from the HTTP request headers
